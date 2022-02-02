eeb01ba7d55ed08cb5df882b5ed1f8795df9d247
nevz <projectnevz.gmail.com>
2022-02-02 18:32:00 +0800

    Finish 80-90% of the project

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# wordle-project
-A Wordle Project made with Basic HTML, CSS, and Javascript
+# Wordle Project
+
+A Wordle Project made with Basic HTML, CSS, and Javscript
+
+Both keyboard and mouse input works to operate
+
+### Preview
+
+![](preview.png)
+
+### Links
+
+- Official Wordle Website: [https://www.powerlanguage.co.uk/wordle/](https://www.powerlanguage.co.uk/wordle/)
+
+- Live Demo: [https://nevz9.github.io/wordle-project/](https://nevz9.github.io/wordle-project/)
+
+- The data is taken from: [https://gist.github.com/cfreshman/cdcdf777450c5b5301e439061d29694c](https://gist.github.com/cfreshman/cdcdf777450c5b5301e439061d29694c)
+
+- VS Code channel's lifesaver tip: [https://www.youtube.com/watch?v=QcXlyriZVa8](https://www.youtube.com/watch?v=QcXlyriZVa8)
+### Built with
+
+- Basic HTML5 and CSS3
+- Javscript Fetch API
+
+##
+
+This is a clone of a popular word guessing game called wordle. You get to guess a 5 letter word in 6 tries. Green indicates that you guessed the correct letter and position in the word, yellow indicates that you guessed the letter correctly but placed in the wrong position, while gray indicates that the letter is not in the word.
+
+This project is still in progress like about 85-90%, but I can't finish it right now. I will try to finish and refine it in the future after I get to know more about javascript.
+
--- script.js
+++ script.js
@@ -1,68 +1,173 @@
-let boxes = document.querySelectorAll('.box')
-let boxIndex = 0
-let wordEnd = 0
-let userInput = []
-let userInputJoin;
-let words = ["apple", "array", "giant", "gator"]
-let answer = []
-let enterFlag = true
-let buttons = document.querySelectorAll('button')
+fetch("./words.json")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    let wordleGrid = document.querySelector('.wordle-grid');
+    let boxes = wordleGrid.children;
+    let words = data.words
+    let buttons = document.querySelectorAll('button');
+    let wordEnd = 0;
+    let row = 0;
+    let col = 0;
+    let userInput = [];
+    let userInputJoin;
+    let answer; // Randomized answer taken fromthe words variable
+    let answerLetterArray; // Converts answer to letters and place them in an array
 
-function randomWord() {
-    let randomizedWord = words[Math.floor(Math.random() * words.length)]
-    answer.push(randomizedWord)
-}
+    function randomizeWord() {
+      let randomizedWord = words[Math.floor(Math.random() * words.length)];
+      answer = randomizedWord.concat("");
+      answerLetterArray = answer.split("");
+    }
 
-randomWord()
+    randomizeWord();
 
-window.addEventListener('keydown', (e) => {
-    let key = document.querySelector(`button[data-key="${e.key}"]`)
-    if (!key) return
-    key.click();
-})
+    // Detects keyboard press
+    window.addEventListener('keydown', (e) => {
+      let key = document.querySelector(`button[data-key="${e.key}"]`);
+      if (!key) return;
+      key.click();
+    })
 
-buttons.forEach(button => button.addEventListener('click', () => {
-    addLetterToBox(button);
-    (button.textContent === 'enter') ? enterWord(): false;
-    (button.textContent === 'del') ? deleteLetter(): false;
-}))
+    // Detects mouse click
+    buttons.forEach(button => button.addEventListener('click', () => {
+      addLetterToBox(button);
+      (button.textContent === 'enter') ? enterWord(): false;
+      (button.textContent === 'del') ? deleteLetter(): false;
 
+      button.classList.add('active');
+      setTimeout(() => {
+        button.classList.remove('active');
+      }, 75)
+    }));
 
-function addLetterToBox(e) {
-    if (e.classList.contains('letter')) {
+    function addLetterToBox(e) {
+      if (e.classList.contains('letter')) {
         if (wordEnd === 5) {
-            return
+          return;
         } else {
-            boxes[boxIndex].textContent += e.textContent
-            boxes[boxIndex].classList.remove('remove')
-            boxes[boxIndex].classList.add('active')
-            userInput.push(e.textContent)
-            boxIndex++
-            wordEnd++
-            userInputJoin = userInput.join("")
+          boxes[row].children[col].textContent = e.textContent;
+          boxes[row].children[col].classList.remove('remove');
+          setTimeout(() => {
+            boxes[row].children[col - 1].classList.add('active');
+          });
+          userInput.push(e.textContent);
+          col++;
+          wordEnd++;
+          userInputJoin = userInput.join("");
         }
+      }
     }
 
-}
+    // Gets the user's input'and checks if it's equal to the answer or not
+    // Otherwise the game continues
+    function enterWord() {
+      wordEnd = 0;
 
-function enterWord() {
-    wordEnd = 0
-    if (answer.includes(userInputJoin)) {
-        console.log("True")
-    } else {
-        userInput = []
-        console.log("False")
+      // Checks if the user puts a 5 character word
+      if (col < 5) {
+        alert("You can only enter a 5 character word!!");
+        for (let i = 0; i < 5; i++) {
+          boxes[row].children[i].textContent = '';
+        }
+      } else {
+        if (userInputJoin === answer) {
+          console.log("YOU WIN !!!!");
+          checkCorrectWord(row);
+        } else {
+          checkGreenBox(userInput, row);
+          checkYellowBox(userInput, row);
+          checkGrayBox(row);
+          userInput = [];
+        }
+      }
+      loadEndScreen();
+      addRow();
+      col = 0;
     }
-}
 
-function deleteLetter() {
-    if (enterFlag) {
-        boxes[boxIndex - 1].classList.remove('active')
-        boxes[boxIndex - 1].classList.add('remove')
-        boxIndex--
-        wordEnd--
-        boxes[boxIndex].textContent = ''
-    } else if (!enterFlag) {
-        return
+    // Checks if the user input is all correct
+    function checkCorrectWord(row) {
+      for (let i = 0; i < 5; i++) {
+        boxes[row].children[i].classList.add('green-box');
+      }
     }
-}
+
+    // Checks if the user input contains the correct letter
+    function checkGreenBox(arr1, row) {
+      let box = boxes[row].children;
+      for (let i = 0; i < 5; i++) {
+        if (answerLetterArray[i] === arr1[i]) {
+          box[i].classList.add('green-box');
+        }
+      }
+    }
+
+    // Checks if the user input contains the correct letter but wrong position
+    function checkYellowBox(arr1, row) {
+      let box = boxes[row].children;
+      for (let i = 0; i < 5; i++) {
+        for (let j = 0; j < 5; j++) {
+          if (answerLetterArray[i] === arr1[j]) {
+            if (!(box[j].classList.contains('green-box'))) {
+              box[j].classList.add('yellow-box');
+            }
+          }
+        }
+      }
+    }
+
+    // Checks if the user input contains wrong letters
+    function checkGrayBox(row) {
+      let box = boxes[row].children;
+      for (let i = 0; i < 5; i++) {
+        if (!(box[i].classList.contains('green-box') || box[i].classList.contains('yellow-box'))) {
+          box[i].classList.add('gray-box');
+        }
+      }
+    }
+
+    function addRow() {
+      (col === 5) ? row += 1: false;
+    }
+
+    // Deletes user input using backspace
+    function deleteLetter() {
+      boxes[row].children[col - 1].classList.add('remove');
+      setTimeout(() => {
+        boxes[row].children[col].classList.remove('active');
+      }, 75);
+
+      col--;
+      wordEnd--;
+      boxes[row].children[col].textContent = '';
+      userInput.pop();
+    }
+
+    let endScreen = document.querySelector('.end-screen');
+    let h1 = document.querySelector('h1');
+    let buttonPlayAgain = document.querySelector('.button');
+
+    function loadEndScreen() {
+      if (userInputJoin === answer) {
+        createEndScreen();
+      } else if (userInputJoin !== answer && row === 5) {
+        h1.textContent = "You didn't guess the word right!";
+        createEndScreen();
+      }
+    }
+
+    function createEndScreen() {
+      endScreen.style.visibility = "visible";
+      setTimeout(() => {
+        endScreen.style.opacity = 1;
+      }, 300);
+
+      buttonPlayAgain.addEventListener('click', () => {
+        window.location.reload();
+      })
+    }
+
+  })
+  .catch(err => console.error(err))
